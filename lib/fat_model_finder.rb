f7158428fa0eb10db99c104b40e5db49788b0724
Max Normand <maxnormand97@gmail.com>
2025-09-10 17:17:55 +0100

    add TOR gem so we can do on the CLI - mock out basic method for TOR to read through one particular domain - make a bin file to execute the new scan

--- lib/fat_model_finder.rb
+++ lib/fat_model_finder.rb
@@ -1,10 +1,36 @@
 # frozen_string_literal: true
 
+require "thor"
 require_relative "fat_model_finder/version"
 
 module FatModelFinder
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Interface for the program to the CLI
+  class CLI < Thor
+    desc "scan DIRECTORY", "Scan a directory in a Rails app"
+    def scan(directory = "app/models")
+      puts "Scanning directory: #{directory}"
+
+      # Check if the directory exists
+      unless Dir.exist?(directory)
+        puts "Directory not found: #{directory}"
+        return
+      end
+
+      # List all files in the directory
+      # when we do the loop here thats what we will have to extract...
+      files = Dir.glob("#{directory}/**/*")
+      files.each do |file|
+        if File.file?(file) # Ensure it's a file and not a directory
+          line_count = File.foreach(file).count
+          puts "#{file} - #{line_count} lines"
+        else
+          puts "#{file} - [Not a file]"
+        end
+      end
+    end
+  end
 
   # Fat model finder
   # It will scan a rails repo specifically in the /models dir
